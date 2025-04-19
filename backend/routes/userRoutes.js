a2a41582594622a3ef8f42d6e91e05277e7b20db
Dev <devpratap2321@gmail.com>
2025-04-19 16:13:05 +0530

    fixed backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,33 +1,73 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt"); // Not used correctly
+const bcrypt = require('bcryptjs')
 const User = require("../models/User");
 
 const router = express.Router();
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  
- 
 
-  const user = new User({ username, password }); 
-  await user.save();
-  res.json({ message: "User registered successfully" });
+const generateToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+  })
+}
+
+router.post("/register", async (req, res) => {
+  const { username, password} = req.body;
+
+  if(!username || !password) {
+    return res.status(400).json({ message: "Username or password cannot be empty"})
+  }
+
+  const user = await User.findOne({ username });
+
+  if (user) {
+    return res.status(400).json({ message: "User already exists!" }); 
+  }
+
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const newUser = await User.create({
+      username: username,
+      password: hashPassword,
+  })
+
+  if(!newUser) {
+    return res.status(400).json({ message: "Failed to create the user" }); 
+  }
+
+  const result = newUser.toJSON();
+
+  delete result.password;
+
+  result.token = generateToken({
+      id: result.id
+  })
+
+    
+  return res.status(201).json({
+      status: 'success',
+      data: result,
+  })
 });
 
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  if(!username || !password) {
+    return res.status(400).json({message: "Username or password cannot be empty"})
+  }
   const user = await User.findOne({ username });
 
   if (!user) {
     return res.status(400).json({ message: "User not found" }); 
   }
 
-  if (user.password !== password) { 
-    return res.status(400).json({ message: "Invalid credentials" });
+  if(!(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({message: "Incorrect Email or password!S"})
   }
 
-  const token = jwt.sign({ userId: user._id }, "insecuresecret", { expiresIn: "1h" }); 
+  const token = generateToken({
+    userId: user._id
+  })
   res.json({ token });
 });
 
